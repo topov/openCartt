7ecc406fca5989e04d732e16286cd84292ad5dae
topov <emil.topov@yahoo.com>
2015-07-21 10:52:32 +0300

    kkkd
    ijoj

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shopnew');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shopnew');
+define('DB_PREFIX', '');
+?>
